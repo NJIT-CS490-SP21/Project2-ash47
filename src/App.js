094552d5193be650d7769ff456bdfd51aa36956f
ahirpara2000 <ec2-user@ip-172-31-50-150.ec2.internal>
2021-02-23 22:53:37 +0000

    Login page

--- src/App.js
+++ src/App.js
@@ -9,9 +9,48 @@ import './Board.css';
 const socket = io(); // Connects to socket connection
 
 function App() {
+  
+  const [users, setUsers ] = useState([]);
+  const inputRef = useRef(null);
+  const [ loginStatus, setLoginStatus ] = useState(false);
+  
+  
+  function changeLogin()
+  {
+    setLoginStatus(currLogin => true);
+  }
+  
+  function onClickAction()
+  {
+    const userText = inputRef.current.value;
+    setUsers(prevList => [...prevList, userText]);
+    changeLogin();
+  }
+  
+  function Login(props)
+  {
+    console.log("Something")
+    const loginStatus = props.isLoggedIn;
+    if(loginStatus)
+      return <Board />
+    else
+      return (
+        <div>
+          <input ref={inputRef} type="text" name="name" />
+          <button onClick= {onClickAction}>Submit</button>
+        </div>
+      );
+      
+    
+  }
+  
+  console.log(users);
+  
   return(
     <center>
-      <Board />
+      
+      <Login isLoggedIn={loginStatus} />
+      
     </center>
   );
   
